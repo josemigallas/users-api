9254582fc22791aae038f1b54a35ce16d0f7ac54
Jose Miguel Gallas Olmedo <jozan@Joses-MacBook-Pro.local>
2017-07-14 13:16:15 +0200

    refactored realm-helper as singleton static class

--- src/repository/realm-helper.ts
+++ src/repository/realm-helper.ts
@@ -5,20 +5,25 @@ import NameSchema from './schema/name-schema';
 import PictureSchema from './schema/picture-schema';
 import UserSchema from './schema/user-schema';
 
-const _config: Realm.Configuration = {
-    path: "database/users",
-    schema: [
-        UserSchema,
-        NameSchema,
-        LocationSchema,
-        PictureSchema
-    ]
-};
-
 export default class RealmHelper {
 
+    private static _realm: Realm;
+
+    private static _config: Realm.Configuration = {
+        path: "database/users",
+        schema: [
+            UserSchema,
+            NameSchema,
+            LocationSchema,
+            PictureSchema
+        ]
+    };
+
     static getDefaultRealm(): Realm {
-        return new Realm(_config);
+        if (!RealmHelper._realm) {
+            RealmHelper._realm = new Realm(RealmHelper._config);
+        }
+        return new Realm(RealmHelper._config);
     }
 
 }
