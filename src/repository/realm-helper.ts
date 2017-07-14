ae5c2342585e722548cfa121274085d86ca8b119
Jose Miguel Gallas Olmedo <jozan@Joses-MacBook-Pro.local>
2017-07-14 18:32:14 +0200

    fixes prestart npm script, populates db with seed data

--- src/repository/realm-helper.ts
+++ src/repository/realm-helper.ts
@@ -1,11 +1,10 @@
-import * as Realm from "realm";
+import * as Realm from 'realm';
 
 import LocationSchema from "./schema/location-schema";
 import NameSchema from "./schema/name-schema";
 import PictureSchema from "./schema/picture-schema";
 import UserSchema from "./schema/user-schema";
-
-import User from "../model/user";
+import User from '../model/user';
 
 export default class RealmHelper {
 
@@ -21,15 +20,40 @@ export default class RealmHelper {
         ]
     };
 
-    static getDefaultRealm(): Realm {
-        if (!RealmHelper._realm) {
-            RealmHelper._realm = new Realm(RealmHelper._config);
+    /**
+     * Singleton getter for the Realm.
+     */
+    private static get defaultRealm(): Realm {
+        if (!this._realm) {
+            this._realm = new Realm(this._config);
         }
-        return new Realm(RealmHelper._config);
+        return new Realm(this._config);
     }
 
+    /**
+     * Initialization method for the Database, intenden to be called when the server app starts up.
+     */
+    public static init(): void {
+        const realm = this.defaultRealm;
+
+        if (!realm.empty) {
+            return;
+        }
+
+        const users: User[] = require("./seed.json").users;
+
+        realm.write(() => {
+            for (const user of users) {
+                realm.create(UserSchema.name, user);
+            }
+        });
+    }
+
+    /**
+     * Gets an array of all users in the realm.
+     */
     static getUsers(): User[] {
-        const results: Realm.Results<User> = RealmHelper.getDefaultRealm()
+        const results: Realm.Results<User> = RealmHelper.defaultRealm
             .objects(UserSchema.name);
 
         return Array.prototype.slice.call(results, 0, results.length);
