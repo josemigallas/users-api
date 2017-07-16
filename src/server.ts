36237c2fc1bbd5dbfc4d7cb040f731855b8ee2fd
Jose Miguel Gallas Olmedo <jozan@Joses-MacBook-Pro.local>
2017-07-16 20:02:29 +0200

    adds GET route with tests

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ app.use(bodyParser.json());
 
 // Routes definitions
 import index from "./routes/index";
+import users from "./routes/users";
 
 app.use("/", index);
+app.use("/users", users);
 
 // Realm configuration
 import RealmHelper from "./repository/realm-helper";
